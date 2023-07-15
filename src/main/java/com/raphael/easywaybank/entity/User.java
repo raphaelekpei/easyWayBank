eec8d63adc8da30cc5b99c53f6ee9e31fa11c614
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-15 20:55:33 +0100

    Commit 110010

---
+++ src/main/java/com/raphael/easywaybank/entity/User.java
@@ -0,0 +1,36 @@
+package com.raphael.easywaybank.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String accountNumber;
+    private BigDecimal accountBalance;
+    private String email;
+    private String phoneNumber;
+    private String alternativePhoneNumber;
+    private String status;
+
+}
