e9e2706148a0549394916a62d9813f0a2a215f51
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-16 06:28:49 +0100

    Commit 110020

--- src/main/java/com/raphael/easywaybank/service/mailSerivce/EmailServiceImpl.java
+++ src/main/java/com/raphael/easywaybank/service/mailSerivce/EmailServiceImpl.java
@@ -1,32 +1,39 @@
-package com.raphael.easywaybank.impl;
+package com.raphael.easywaybank.service.mailSerivce;
 
-import com.musdon.thejavaacademybank.dto.EmailDetails;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.raphael.easywaybank.dto.request.EmailDetails;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class EmailServiceImpl implements EmailService{
 
-    @Autowired
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
 
     @Value("${spring.mail.username}")
     private String senderEmail;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
 
     @Override
     public void sendEmail(EmailDetails emailDetails) {
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage
-                    .setFrom(senderEmail)
-                    .setTo(emailDetails.getRecipient())
-                    .setText(emailDetails.getMessageBody())
-                    .setSubject(emailDetails.getSubject());
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setSubject(emailDetails.getSubject());
+            mailMessage.setText(emailDetails.getMessageBody());
 
             javaMailSender.send(mailMessage);
 
@@ -34,6 +41,7 @@ public class EmailServiceImpl implements EmailService{
             throw new RuntimeException(e);
         }
     }
+
 
     @Override
     public void sendEmailWithAttachment(EmailDetails emailDetails) {
@@ -51,6 +59,7 @@ public class EmailServiceImpl implements EmailService{
             mimeMessageHelper.addAttachment(file.getFilename(), file);
 
             javaMailSender.send(mimeMessage);
+
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
