ead29c1fe03cc80f59572b5f89da8cecac28536f
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-16 06:28:28 +0100

    Commit 110019

--- src/main/java/com/raphael/easywaybank/service/mailSerivce/EmailService.java
+++ src/main/java/com/raphael/easywaybank/service/mailSerivce/EmailService.java
@@ -1,6 +1,7 @@
-package com.raphael.easywaybank.impl;
+package com.raphael.easywaybank.service.mailSerivce;
 
-import com.musdon.thejavaacademybank.dto.EmailDetails;
+
+import com.raphael.easywaybank.dto.request.EmailDetails;
 
 public interface EmailService {
     void sendEmail(EmailDetails emailDetails);
